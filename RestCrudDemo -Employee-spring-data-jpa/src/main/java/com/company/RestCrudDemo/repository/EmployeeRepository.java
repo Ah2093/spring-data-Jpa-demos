6e52576532fa402a68c1d8a5c0abbcad5c0a9520
Ahmed Hamdi <ah2093@fayoum.edu.eg>
2023-08-02 15:37:30 +0300

    refactor some code in controller

--- RestCrudDemo -Employee-spring-data-jpa/src/main/java/com/company/RestCrudDemo/repository/EmployeeRepository.java
+++ RestCrudDemo -Employee-spring-data-jpa/src/main/java/com/company/RestCrudDemo/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.company.RestCrudDemo.repository;
 
 import com.company.RestCrudDemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 }
